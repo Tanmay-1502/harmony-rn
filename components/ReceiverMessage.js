2287f71ac346dc7665cacaf7192da56574a0603a
TANMAY SUKHIJA <63812317+Tanmay-1502@users.noreply.github.com>
2022-12-17 11:48:13 -0800

    Update ReceiverMessage.js

--- components/ReceiverMessage.js
+++ components/ReceiverMessage.js
@@ -1,18 +1,20 @@
 import React from "react";
 import { View, Text, Image, TouchableOpacity } from "react-native";
 import tw from "tailwind-rn";
-// import * as Speech from 'expo-speech';
+ import * as Speech from 'expo-speech';
 
 
-// const speak = () => {
-//   const thingToSay = 'This is the latest message';
-//   Speech.speak(thingToSay);
-// };
+
 const ReceiverMessage = ({ message }) => {
+    const speak = () => {
+    const thingToSay = message.message;
+    Speech.speak(thingToSay);
+    };
   return (
+   
     <View
       style={[
-        tw("bg-pink-600 rounded-lg rounded-tr-none px-5 py-3 mx-14 my-4"),
+        tw("bg-black rounded-lg rounded-tr-none px-5 py-3 mx-14 my-4"), //pink-600
         { alignSelf: "flex-start" }, 
       ]}
     >
@@ -20,9 +22,9 @@ const ReceiverMessage = ({ message }) => {
         style={tw("h-12 w-12 rounded-full absolute top-0 -left-14")}
         source={{ uri: message.photoURL }}
       />
-      {/* <TouchableOpacity style={tw("text-white")} onPress={speak}> */}
+      <TouchableOpacity style={tw("text-white")} onPress={speak}>
       <Text style={tw("text-white")}>{message.message}</Text>
-      {/* </TouchableOpacity> */}
+      </TouchableOpacity>
     </View>
   );
 };
