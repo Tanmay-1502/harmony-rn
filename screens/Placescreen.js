62ba55122a1db09afee4fa657e3486b43c05470a
TANMAY SUKHIJA <63812317+Tanmay-1502@users.noreply.github.com>
2022-12-17 11:47:20 -0800

    Update Placescreen.js

--- screens/Placescreen.js
+++ screens/Placescreen.js
@@ -2,76 +2,79 @@ import { StyleSheet, Text, View, FlatList , SectionList,TextInput, ScrollView, I
 import React, { useState, useEffect } from 'react';
 import tw from "tailwind-rn";
 import filter from 'lodash.filter';
-
+import places from "../test.json";
 const place= require('../places.json');
 const Placescreen = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [data, setData] = useState([]);
-  const [error, setError] = useState(null);
-  const API_ENDPOINT = `https://randomuser.me/api/?seed=1&page=1&results=20`;
-  const [query, setQuery] = useState('');
-const [fullData, setFullData] = useState([]);
-useEffect(() => {
-  //setIsLoading(true);
 
-  fetch(API_ENDPOINT)
-    .then(response => response.json())
-    .then(response => {
-      setData(response.results);
+  const [filterdData, setfiltered]=useState([]);
+    const [masterData, setmasterData]=useState([]);
+   // console.log(places)
+     const [search, setSearch]=useState('');
+    //  useEffect(()=>{
+    //     if(search.length!=0)
+    //         fetchPosts();
+    //     return()=>{
 
-      // ADD THIS
-      setFullData(response.results);
+    //     }
+    //  },[])
+     useEffect(()=>{
+        fetchPosts();
+        return()=>{
 
-   //   setIsLoading(false);
-    })
-    .catch(err => {
-    //  setIsLoading(false);
-      setError(err);
-    });
-}, []);
-const handleSearch = text => {
-  const formattedQuery = text.toLowerCase();
-  const filteredData = filter(fullData, user => {
-    return contains(user, formattedQuery);
-  });
-  setData(filteredData);
-  setQuery(text);
-};
+        }
+     },[])
+    const fetchPosts=()=>{
+    //    const apiURL="https://jsonplaceholder.typicode.com/posts";
+    
 
-const contains = ({ name, email }, query) => {
-  const { first, last } = name;
+    //    fetch(apiURL)
 
-  if (first.includes(query) || last.includes(query) || email.includes(query)) {
-    return true;
-  }
+    //     .then((response)=>response.json())
+    //     .then((responseJson)=>{
+    //         setfiltered(responseJson);
+    //         setmasterData(responseJson);
+    //     }).catch((error)=>{
+    //         console.error(error);
+    //     })
+            setfiltered(places);
+            setmasterData(places);
+    }
 
-  return false;
+    const searchFilter=(text)=>{
+        if(text){
+            const newData=masterData.filter((item)=>{
+                const itemData=item.title?
+                item.title.toUpperCase()
+                :''.toUpperCase();
+            const textData=text.toUpperCase();
+            return itemData.indexOf(textData)> -1;
+            })
+            setfiltered(newData);
+            setSearch(text);
+        } else{
+            // <Text>
+            //     hello
+            // </Text>
+            setfiltered(masterData);
+            setSearch(text);
+        }
+    }
+    const ItemView=({item})=>{
+        return(
+           
+            <Text style={styles.itemStyle}>
+            {item.id}{' '}{item.title.toUpperCase()}
+            </Text>
+        )
+    }
+    const ItemSeparatorView=()=>{
+        return(
+            <View style={{height:0.5, width:'100%', backgroundColor:'#c8c8c8'}}/>
 
+     
+        )
+    }
 
-};
-
-function renderHeader() {
-  return (
-    <View
-      style={{
-        backgroundColor: '#fff',
-        padding: 10,
-        marginVertical: 10,
-        borderRadius: 20
-      }}
-    >
-      <TextInput
-        autoCapitalize="none"
-        autoCorrect={false}
-        clearButtonMode="always"
-        value={query}
-        onChangeText={queryText => handleSearch(queryText)}
-        placeholder="Search"
-        style={{ backgroundColor: '#fff', paddingHorizontal: 20 }}
-      />
-    </View>
-  );
-}
   return (
     <View style={tw(" flex-1 bg-red-200 mt-7")}>
       <ImageBackground
@@ -83,30 +86,39 @@ function renderHeader() {
       >
       <Text style={tw("text-center m-5 text-3xl")}>Wheelchair Accessible Hotels and Restaurants</Text>
     
+      <TextInput style={styles.textInputStyle}
+         value={search} 
+         placeholder="Search Here"
+         underlineColorAndroid="transparent"
+         onChangeText={(text)=>searchFilter(text)}/>
+         
+           <FlatList  style={styles.flat} data={filterdData}
+           keyExtractor={(item, index)=>index.toString()}
+           ItemSeparatorComponent={ItemSeparatorView}
+           renderItem={ItemView} />
 
-<ScrollView StickyHeaderComponent={renderHeader} >
-
-
+<ScrollView style={styles.test} >
 <Text style={tw("text-center m-5 text-2xl")}>KARNAL</Text>
-      {place.map((places) =>(
-         <Text style={tw("text-xl")} key={places.id}>{places.KARNAL}</Text>
+      {place.map((places, id) =>(
+         <Text style={tw("text-xl")} key={id}>{places.KARNAL}</Text>
         // <Text key={places.KARNAL}> list of Accessible places in karnal</Text>
         
       ))}
       <Text style={tw("text-center m-5 text-2xl")}>MUMBAI</Text>
-      {place.map((places) =>(
-        <Text style={tw("text-xl")} key={places.MUMBAI}>{places.MUMBAI}</Text>
+      {place.map((places, id) =>(
+        <Text style={tw("text-xl")} key={id}>{places.MUMBAI}</Text>
         
 
         
       ))}
       <Text style={tw("text-center m-5 text-2xl")}>DELHI</Text>
-      {place.map((places) =>(
-        <Text style={tw("text-xl")} key={places['NEW DELHI']}>{places['NEW DELHI']}</Text>
+      {place.map((places, id) =>(
+        <Text style={tw("text-xl")} key={id}>{places['NEW DELHI']}</Text>
         
 
         
-      ))}<Text style={tw("text-center m-5 text-2xl ")}>GURUGRAM</Text>
+      ))}
+      {/* <Text style={tw("text-center m-5 text-2xl ")}>GURUGRAM</Text>
       {place.map((places) =>(
         <Text style={tw("text-xl")} key={places.GURUGRAM}>{places.GURUGRAM}</Text>
         
@@ -234,24 +246,24 @@ function renderHeader() {
       
 
         
-      ))}
+      ))} */}
       <Text style={tw("text-center m-5 text-2xl")}>SURAT</Text>
-      {place.map((places) =>(
-        <Text style={tw("text-xl")} key={places.SURAT}>{places.SURAT}</Text>
+      {place.map((places, id) =>(
+        <Text style={tw("text-xl")} key={id}>{places.SURAT}</Text>
       
 
         
       ))}
       <Text style={tw("text-center m-5 text-2xl")}>VADODRA</Text>
-      {place.map((places) =>(
-        <Text style={tw("text-xl")} key={places.VADODRA}>{places.VADODRA}</Text>
+      {place.map((places, id) =>(
+        <Text style={tw("text-xl")} key={id}>{places.VADODRA}</Text>
       
 
         
       ))}
       <Text style={tw("text-center m-5 text-2xl")}>VISHAKHAPATNAM</Text>
-      {place.map((places) =>(
-        <Text style={tw("text-xl")} key={places.VISHAKHAPATNAM}>{places.VISHAKHAPATNAM}</Text>
+      {place.map((places, id) =>(
+        <Text style={tw("text-xl")} key={id}>{places.VISHAKHAPATNAM}</Text>
       
 
         
@@ -272,6 +284,12 @@ const styles = StyleSheet.create({
      flex: 1,
      paddingTop: 22
     },
+    flat:{
+     height: 10
+    },
+    test:{
+      height: 50
+    },
     sectionHeader: {
       paddingTop: 2,
       paddingLeft: 10,
@@ -286,6 +304,22 @@ const styles = StyleSheet.create({
       fontSize: 18,
       height: 44,
     },
+    itemStyle:{
+      padding:15,
+   
+  },
+  textInputStyle:{
+      height: 50,
+        borderWidth: 1,
+      paddingLeft:20,
+      margin:5,
+      borderColor:'#009688',
+      backgroundColor: 'white'
+  }
+
   })
   
+
+
+
 
