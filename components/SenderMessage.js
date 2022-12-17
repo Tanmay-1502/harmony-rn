21fef6f33ea0355ef19f15f5dd7bb79c6185b429
TANMAY SUKHIJA <63812317+Tanmay-1502@users.noreply.github.com>
2022-12-17 11:48:56 -0800

    Update SenderMessage.js

--- components/SenderMessage.js
+++ components/SenderMessage.js
@@ -1,14 +1,10 @@
-import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 import tw from "tailwind-rn";
 import * as Speech from 'expo-speech';
+import { collection, onSnapshot, orderBy, query } from "@firebase/firestore";
+import React,{useState, useEffect} from "react";
+import getMatchedUserInfo from "../lib/getMatchedUserInfo";
 
-
-const speak = () => {
-  
-  const thingToSay = 'Hello';
-  Speech.speak(thingToSay);
-};
 // const real = ({ message }) => {
 //   a=message.message;
 //   const thingToSay = {a};
@@ -16,10 +12,30 @@ const speak = () => {
 // };
 
 const SenderMessage = ({ message }) => {
+//   const [lastMessage, setLastMessage] = useState("");
+// useEffect(
+//   () =>
+//     onSnapshot(
+//       query(
+//         collection(db, "matches", matchDetails.id, "messages"),
+//         orderBy("timestamp", "desc")
+//       ),
+//       (snapshot) => {
+//         setLastMessage(snapshot.docs[0]?.data().message);
+//       } // 0th index as we used desc order in CHATS
+//       //using ? because if there is no message, it will be undefined
+//     ),
+//   [matchDetails, db]
+// );
+const speak = () => {
+  
+  const thingToSay = message.message;
+  Speech.speak(thingToSay);
+};
   return (
     <View
       style={[
-        tw("bg-purple-600 rounded-lg rounded-tr-none px-5 py-3 mx-3 my-4"),
+        tw("bg-gray-500 rounded-lg rounded-tr-none px-5 py-3 mx-3 my-4"),
         { alignSelf: "flex-start", marginLeft: "auto" }, 
       ]}
     >
