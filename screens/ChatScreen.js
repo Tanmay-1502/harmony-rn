3e5549df714a7f982f9126621d11a4b5603f79ee
TANMAY SUKHIJA <63812317+Tanmay-1502@users.noreply.github.com>
2022-12-17 11:35:58 -0800

    Update ChatScreen.js

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -7,49 +7,85 @@ import * as Speech from 'expo-speech';
 import tw from "tailwind-rn";
 import { TouchableOpacity } from "react-native";
 import {Ionicons}  from "@expo/vector-icons";
+import { MaterialCommunityIcons , FontAwesome} from '@expo/vector-icons'; 
 
 
 const ChatScreen = () => {
+  const stop = () => {
+    
+    Speech.stop();
+    };
   const navigation = useNavigation();
   const speak = () => {
     const thingToSay = 'This is the chatscreen. You can chat with the people who have matched with you. To chat, just press the image of that person';
     Speech.speak(thingToSay);
   };
-
+  const speak1 = () => {
+    const thingToSay = 'यह चैट स्क्रीन है। आप उन लोगों के साथ बातें कर सकते हैं जो आपके साथ मेल खाते हैं। चैट करने के लिए, बस उस व्यक्ति की छवि दबाएं';
+    Speech.speak(thingToSay, {language: "hin"});
+  };
+ 
   return (
     
       
-     <View style={tw("bg-red-200 mt-7")}>
+     <View style={tw(" flex flex-1  mt-7")}>
       <ImageBackground
        source={require("../imp.jpg")}
         resizeMode="cover"
-       style={tw(" w-full")}>
+       style={tw("h-full w-full")}>
 
       
-       <View style={tw("")} >
+       <View style={tw("flex flex-row justify-between")} >
        
-       <TouchableOpacity onPress={speak} style={tw("flex-row justify-between items-center pr-5")} >
+       
+        
        <Header title="Chat" />
-       <Ionicons name="star" size={30} color="black" />
-       </TouchableOpacity>
+       {/* <View style={tw("flex flex-row justify-between bg-black m-1 p-3 rounded-full")}>
+       
+      
+       </View> */}
+
+       
+       
      
        </View>
       
       
-       
+       {/* <Text>hello</Text> */}
+
       
       
 
       <ChatList />
-      
-      <Button
+      {/* <Text>hello</Text> */}
+      {/* <Button
         title="Go Back to Home Screen"
         onPress={() => navigation.goBack()}
-      />
-     </ImageBackground>
+      /> */}
+
+<View style={tw("flex flex-row  justify-between bg-black bottom-0")}>
+        <TouchableOpacity   onPress={speak}>
+           {/* <Ionicons name="star" size={30} color="black" /> */}
+           <MaterialCommunityIcons style={tw("mx-3 mb-1")} name="alpha-e-box" size={40} color="white" />
+           </TouchableOpacity>
+           {/* <TouchableOpacity
+           style={tw("rounded-full p-1   ")}
+        
+          onPress={stop}>
+          <FontAwesome name="square" size={34} color="white" />
+        </TouchableOpacity> */}
+
+           <TouchableOpacity   onPress={speak1}>
+           {/* <Ionicons name="star" size={30} color="black" /> */}
+           <MaterialCommunityIcons style={tw("mx-3 mb-1")} name="alpha-h-box" size={40} color="white" />
+           </TouchableOpacity>
+         </View>
+
+      </ImageBackground>
     </View>
   );
 };
 
 export default ChatScreen;
+
 
